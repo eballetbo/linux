9e0f62582b0a844107a5997bdb59f405becaabe3
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2020-09-25 11:42:26 +0200

    Revert "soc: mediatek: mmsys: Create struct mtk_mmsys to store context data"
    This reverts commit 70ea329dedd1c830b6c9c5a2932d6b3d0f5901e9.

--- drivers/soc/mediatek/mtk-mmsys.c
+++ drivers/soc/mediatek/mtk-mmsys.c
@@ -99,9 +99,8 @@ static const struct mtk_mmsys_driver_data mt8173_mmsys_driver_data = {
 	.clk_driver = "clk-mt8173-mm",
 };
 
-struct mtk_mmsys {
-	void __iomem *regs;
-	const struct mtk_mmsys_driver_data *data;
+static const struct mtk_mmsys_driver_data mt8183_mmsys_driver_data = {
+	.clk_driver = "clk-mt8183-mm",
 };
 
 static unsigned int mtk_mmsys_ddp_mout_en(enum mtk_ddp_comp_id cur,
@@ -262,21 +261,21 @@ void mtk_mmsys_ddp_connect(struct device *dev,
 			   enum mtk_ddp_comp_id cur,
 			   enum mtk_ddp_comp_id next)
 {
-	struct mtk_mmsys *mmsys = dev_get_drvdata(dev);
+	void __iomem *config_regs = dev_get_drvdata(dev);
 	unsigned int addr, value, reg;
 
 	value = mtk_mmsys_ddp_mout_en(cur, next, &addr);
 	if (value) {
-		reg = readl_relaxed(mmsys->regs + addr) | value;
-		writel_relaxed(reg, mmsys->regs + addr);
+		reg = readl_relaxed(config_regs + addr) | value;
+		writel_relaxed(reg, config_regs + addr);
 	}
 
-	mtk_mmsys_ddp_sout_sel(mmsys->regs, cur, next);
+	mtk_mmsys_ddp_sout_sel(config_regs, cur, next);
 
 	value = mtk_mmsys_ddp_sel_in(cur, next, &addr);
 	if (value) {
-		reg = readl_relaxed(mmsys->regs + addr) | value;
-		writel_relaxed(reg, mmsys->regs + addr);
+		reg = readl_relaxed(config_regs + addr) | value;
+		writel_relaxed(reg, config_regs + addr);
 	}
 }
 EXPORT_SYMBOL_GPL(mtk_mmsys_ddp_connect);
@@ -285,49 +284,47 @@ void mtk_mmsys_ddp_disconnect(struct device *dev,
 			      enum mtk_ddp_comp_id cur,
 			      enum mtk_ddp_comp_id next)
 {
-	struct mtk_mmsys *mmsys = dev_get_drvdata(dev);
+	void __iomem *config_regs = dev_get_drvdata(dev);
 	unsigned int addr, value, reg;
 
 	value = mtk_mmsys_ddp_mout_en(cur, next, &addr);
 	if (value) {
-		reg = readl_relaxed(mmsys->regs + addr) & ~value;
-		writel_relaxed(reg, mmsys->regs + addr);
+		reg = readl_relaxed(config_regs + addr) & ~value;
+		writel_relaxed(reg, config_regs + addr);
 	}
 
 	value = mtk_mmsys_ddp_sel_in(cur, next, &addr);
 	if (value) {
-		reg = readl_relaxed(mmsys->regs + addr) & ~value;
-		writel_relaxed(reg, mmsys->regs + addr);
+		reg = readl_relaxed(config_regs + addr) & ~value;
+		writel_relaxed(reg, config_regs + addr);
 	}
 }
 EXPORT_SYMBOL_GPL(mtk_mmsys_ddp_disconnect);
 
 static int mtk_mmsys_probe(struct platform_device *pdev)
 {
-	struct mtk_mmsys *mmsys;
+	const struct mtk_mmsys_driver_data *data;
 	struct device *dev = &pdev->dev;
 	struct platform_device *clks;
 	struct platform_device *drm;
+	void __iomem *config_regs;
 	struct resource *mem;
 	int ret;
 
-	mmsys = devm_kzalloc(dev, sizeof(*mmsys), GFP_KERNEL);
-	if (!mmsys)
-		return -ENOMEM;
-
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	mmsys->regs = devm_ioremap_resource(dev, mem);
-	if (IS_ERR(mmsys->regs)) {
-		ret = PTR_ERR(mmsys->regs);
+	config_regs = devm_ioremap_resource(dev, mem);
+	if (IS_ERR(config_regs)) {
+		ret = PTR_ERR(config_regs);
 		dev_err(dev, "Failed to ioremap mmsys-config resource: %d\n",
 			ret);
 		return ret;
 	}
 
-	mmsys->data = of_device_get_match_data(&pdev->dev);
-	platform_set_drvdata(pdev, mmsys);
+	platform_set_drvdata(pdev, config_regs);
 
-	clks = platform_device_register_data(&pdev->dev, mmsys->data->clk_driver,
+	data = of_device_get_match_data(&pdev->dev);
+
+	clks = platform_device_register_data(&pdev->dev, data->clk_driver,
 					     PLATFORM_DEVID_AUTO, NULL, 0);
 	if (IS_ERR(clks))
 		return PTR_ERR(clks);
